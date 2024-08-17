ae0f16b2c0bb57f3b7167920bcc0b5aa1486670d
Imaad Schuller <imaadschullerr@gmail.com>
2024-08-17 22:58:42 +0200

    edit views.py to import login

--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -5,7 +5,7 @@ from django.contrib.auth.forms import UserCreationForm
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import views as auth_views
-
+from django.contrib.auth import login
 #List View
 def list_books(request):
     books = Book.objects.all()
