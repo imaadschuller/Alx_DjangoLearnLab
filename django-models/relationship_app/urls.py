99ac9d9cd03ebbfd7c29bd5d6d14c5f12339bb4e
Imaad Schuller <imaadschullerr@gmail.com>
2024-08-17 22:45:40 +0200

    Edit views.py to reflect correct path

--- django-models/relationship_app/urls.py
+++ django-models/relationship_app/urls.py
@@ -1,1 +1,10 @@
+from django.conf.urls import url
+from django.contrib import admin
+from django.urls import path
+from ..relationship_app.views import list_books, LibraryDetailView
 
+urlpatterns = [
+    url(r"^admin/", admin.site.urls),
+    path('books/', list_books, name='list_books'),
+    path('library/<int:pk>/', LibraryDetailView.as_view(), name='library_detail'),
+]
