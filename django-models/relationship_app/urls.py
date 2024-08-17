44f1806fe3b221f0ac933ff0662acf0bf75e42ed
Imaad Schuller <imaadschullerr@gmail.com>
2024-08-17 23:25:52 +0200

    Implement role-based access control: added UserProfile model and views

--- django-models/relationship_app/urls.py
+++ django-models/relationship_app/urls.py
@@ -1,7 +1,7 @@
 from django.conf.urls import url
 from django.contrib import admin
 from django.urls import path
-from .views import list_books, LibraryDetailView, register, LoginView, LogoutView
+from .views import list_books, LibraryDetailView, register, LoginView, LogoutView, admin_view, librarian_view, member_view
 
 urlpatterns = [
     url(r"^admin/", admin.site.urls),
@@ -10,4 +10,7 @@ urlpatterns = [
     path('login/', LoginView.as_view(template_name='login.html'), name='login'),
     path('logout/', LogoutView.as_view(template_name='logout.html'), name='logout'),
     path('register/', views.register, name='register'),
+     path('admin/', admin_view(template_name=admin_view.html), name='admin_view'),
+    path('librarian/', librarian_view(template_name=librarian_view.html), name='librarian_view'),
+    path('member/', member_view(template_name=member_view.html), name='member_view'),
 ]
--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -6,6 +6,7 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import views as auth_views
 from django.contrib.auth import login
+from django.contrib.auth.decorators import user_passes_test
 #List View
 def list_books(request):
     books = Book.objects.all()
@@ -38,3 +39,24 @@ class LoginView(auth_views.LoginView):
 # User Logout View
 class LogoutView(auth_views.LogoutView):
     template_name = 'relationship_app/logout.html'
+
+def is_admin(user):
+    return user.userprofile.role == 'Admin'
+
+def is_librarian(user):
+    return user.userprofile.role == 'Librarian'
+
+def is_member(user):
+    return user.userprofile.role == 'Member'
+
+@user_passes_test(is_admin)
+def admin_view(request):
+    return render(request, 'admin_view.html')
+
+@user_passes_test(is_librarian)
+def librarian_view(request):
+    return render(request, 'librarian_view.html')
+
+@user_passes_test(is_member)
+def member_view(request):
+    return render(request, 'member_view.html')
