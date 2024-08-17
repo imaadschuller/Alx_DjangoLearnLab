eb570e5f49f040905e549dfae6307a96c98997a4
Imaad Schuller <imaadschullerr@gmail.com>
2024-08-17 22:47:55 +0200

    edit views.py to reflect correct path for class based view

--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -10,6 +10,6 @@ from .models import Library
 
 class LibraryDetailView(DetailView):
     model = Library
-    template_name = 'library_detail.html'
+    template_name = 'relationship_app/library_detail.html'
     context_object_name = 'library'
 
