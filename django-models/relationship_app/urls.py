1937b96c883674ca1b2a8bdd7f31d1815d5f1db5
Imaad Schuller <imaadschullerr@gmail.com>
2024-08-17 22:49:33 +0200

    edit urls.py to include from .views

--- django-models/relationship_app/urls.py
+++ django-models/relationship_app/urls.py
@@ -1,7 +1,7 @@
 from django.conf.urls import url
 from django.contrib import admin
 from django.urls import path
-from ..relationship_app.views import list_books, LibraryDetailView
+from .views import list_books, LibraryDetailView
 
 urlpatterns = [
     url(r"^admin/", admin.site.urls),
