a7d53ec82a7f431c5498fa0da3622b17a797acfe
Imaad Schuller <imaadschullerr@gmail.com>
2024-08-17 23:03:18 +0200

    edit urls.py to include template_name for LoginView, LogoutView

--- django-models/relationship_app/urls.py
+++ django-models/relationship_app/urls.py
@@ -7,7 +7,7 @@ urlpatterns = [
     url(r"^admin/", admin.site.urls),
     path('books/', list_books, name='list_books'),
     path('library/<int:pk>/', LibraryDetailView.as_view(), name='library_detail'),
-    path('login/', LoginView.as_view(), name='login'),
-    path('logout/', LogoutView.as_view(), name='logout'),
+    path('login/', LoginView.as_view(template_name='login.html'), name='login'),
+    path('logout/', LogoutView.as_view(template_name='logout.html'), name='logout'),
     path('register/', register, name='register'),
 ]
