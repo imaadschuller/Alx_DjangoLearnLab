8a15de77eb925cdfe93f6f2d62e72c2f08495213
Imaad Schuller <imaadschullerr@gmail.com>
2024-08-25 20:48:44 +0200

    add exampleform to forms.py

--- advanced_features_and_security/LibraryProject/bookshelf/forms.py
+++ advanced_features_and_security/LibraryProject/bookshelf/forms.py
@@ -8,3 +8,8 @@ class BookForm(forms.ModelForm):
 
 class BookSearchForm(forms.Form):
     query = forms.CharField(max_length=100)
+
+class ExampleForm(forms.Form):
+    name = forms.CharField(max_length=100, required=True)
+    email = forms.EmailField(required=True)
+    message = forms.CharField(widget=forms.Textarea, required=True)
