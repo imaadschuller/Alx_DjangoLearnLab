69325be4875fcb4d2fe2c5df61862c7381ba0d1b
Imaad Schuller <imaadschullerr@gmail.com>
2024-09-12 11:19:02 +0200

    Changed database configuration from SQLite3 to MySQL

--- django_blog/django_blog/settings.py
+++ django_blog/django_blog/settings.py
@@ -9,7 +9,8 @@ https://docs.djangoproject.com/en/5.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
-
+import pymysql
+pymysql.install_as_MySQLdb()
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -76,8 +77,12 @@ WSGI_APPLICATION = 'django_blog.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': "Django_Blog",
+        'USER': "root",
+        "PASSWORD": "imaadSCHULL3R",
+        "HOST": "localhost",
+        "PORT": "3306"
     }
 }
 
