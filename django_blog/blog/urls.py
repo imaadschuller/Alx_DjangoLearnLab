f0b22c9ccd47f42c2cef5588ecad703e33f94672
Imaad Schuller <imaadschullerr@gmail.com>
2024-09-15 18:16:02 +0200

    Implement CRUD features for blog posts with templates and views

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from django.contrib.auth import views as auth_views
 from . import views
-
+from .views import PostListView, PostCreateView, PostDeleteView, PostDetailView, PostUpdateView
 urlpatterns = [
     path("login/", auth_views.LoginView.as_view(template_name="blog/registration/login.html"), name="login"),
     path("logout/", auth_views.LogoutView.as_view(template_name="blog/registration/logout.html"), name="logout"),
@@ -9,4 +9,9 @@ urlpatterns = [
     path("register/", views.register, name="register"),
     path("", views.home, name="home"),
     path("posts/", views.posts, name="posts"),
+    path('', PostListView.as_view(), name='post-list'),
+    path('post/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
+    path('post/new/', PostCreateView.as_view(), name='post-create'),
+    path('post/<int:pk>/edit/', PostUpdateView.as_view(), name='post-update'),
+    path('post/<int:pk>/delete/', PostDeleteView.as_view(), name='post-delete'),
 ]
--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -4,6 +4,11 @@ from django.contrib.auth import login as auth_login
 from .forms import CustomUserCreationForm
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.forms import UserChangeForm
+from django.urls import reverse_lazy
+from django.views.generic import ListView,DetailView,CreateView,UpdateView,DeleteView
+from .models import Post
+from .forms import PostForm
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 
 # Custom Registration Form with Auto-Login
 def custom_register(request):
@@ -47,4 +52,40 @@ def profile(request):
     else:
         form = UserChangeForm(instance=request.user)
 
-    return render(request, "templates/profile.html", {"form": form})
+    return render(request, "blog/profile.html", {"form": form})
+
+# Blog Post Crud Operations
+class PostListView(ListView):
+    model = Post
+    template_name = "blog/post_list.html"
+
+class PostDetailView(DetailView):
+    model = Post
+    template_name = "blog/post_detail.html"
+
+class PostCreateView(LoginRequiredMixin, CreateView):
+    model = Post
+    form_class = PostForm
+    template_name = 'blog/post_form.html'
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+    model = Post
+    form_class = PostForm
+    template_name = 'blog/post_form.html'
+
+    def test_func(self):
+        post = self.get_object()
+        return self.request.user == post.author
+
+class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+    model = Post
+    template_name = 'blog/post_confirm_delete.html'
+    success_url = reverse_lazy('post-list')
+
+    def test_func(self):
+        post = self.get_object()
+        return self.request.user == post.author
