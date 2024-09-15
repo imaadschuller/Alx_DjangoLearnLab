c16607866acbe4b061e80057384759f1eb9ea38a
Imaad Schuller <imaadschullerr@gmail.com>
2024-09-15 19:13:45 +0200

    Correct spelling error in views.py

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -139,7 +139,7 @@ def search_posts(request):
     query = request.GET.get("q")
     if query:
         posts = Post.objects.filter(
-            Q(title__icontains=query) | Q(conent__icontains=query) | Q(tags__name__icontains=query)
+            Q(title__icontains=query) | Q(content__icontains=query) | Q(tags__name__icontains=query)
         ).distinct()
     else:
         posts = Post.objects.none()
