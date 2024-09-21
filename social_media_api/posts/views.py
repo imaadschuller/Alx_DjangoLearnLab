7737f9b1d4542a7b24dcec540ac726b9c81609d2
Imaad Schuller <imaadschullerr@gmail.com>
2024-09-21 22:38:33 +0200

    Implement posts and comments features with models, serializers and views

---
+++ social_media_api/posts/views.py
@@ -0,0 +1,32 @@
+from django.shortcuts import render
+from rest_framework import viewsets, permissions
+from .models import Post, Comment
+from .serializers import PostSerializer, CommentSerializer
+from django_filters import rest_framework as filters
+
+class PostFilter(filters.FilterSet):
+    title = filters.CharFilter(lookup_expr='icontains')
+    content = filters.CharFilter(lookup_expr='icontains')
+
+    class Meta:
+        model = Post
+        fields = ['title', 'content']
+
+class PostViewSet(viewsets.ModelViewSet):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+    filter_backends = (filters.DjangoFilterBackend,)
+    filterset_class = PostFilter
+    permission_classes = [permissions.IsAuthenticated]
+
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user)
+
+class CommentViewSet(viewsets.ModelViewSet):
+    queryset = Comment.objects.all()
+    serializer_class = CommentSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user)
+
--- social_media_api/social_media_api/settings.py
+++ social_media_api/social_media_api/settings.py
@@ -38,10 +38,24 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'accounts',
+    'posts',
+    'rest_framework'
+    'rest_framework.authtoken'
 ]
 
 # Custom User Model
 AUTH_USER_MODEL = 'accounts.CustomUser'
+
+REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 10,
+}
+
+REST_FRAMEWORK = {
+    'DEFAULT_FILTER_BACKENDS': (
+        'django_filters.rest_framework.DjangoFilterBackend',
+    ),
+}
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
