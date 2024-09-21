e6d5ec2a7728ea0eab6012bdf65886b9c3567e13
Imaad Schuller <imaadschullerr@gmail.com>
2024-09-21 20:39:00 +0200

    Add login url pattern to accounts app for user authentication

--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -1,6 +1,7 @@
 from rest_framework import serializers
 from django.contrib.auth import get_user_model
 from rest_framework.authtoken.models import Token
+from django.core.exceptions import ValidationError
 
 User = get_user_model()
 
@@ -8,6 +9,9 @@ class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ("id", "username", "bio", "profile_picture")
+        extra_kwargs = {
+            'password': {'write_only': True}
+        }
 
 class RegisterSerializer(serializers.ModelSerializer):
     class Meta:
@@ -15,8 +19,21 @@ class RegisterSerializer(serializers.ModelSerializer):
         fields = ("username", "password", "bio", "profile_picture")
 
         def create(self, validated_data):
-            user = User(**validated_data)
-            user.set_password(validated_data["password"])
-            user.save()
+            user = User.objects.create_user(
+                username=validated_data["username"],
+                password=validated_data["password"],
+                bio=validated_data.get("bio"),
+                profile_picture=validated_data.get("profile_picture")
+            )
             Token.objects.create(user=user)
             return user
+
+class LoginSerializer(serializers.Serializer):
+    username = serializers.CharField(required=True)
+    password = serializers.CharField(required=True)
+
+    def validate(self, data):
+        user = User.objects.filter(username=data['username']).first()
+        if user is None or not user.check_password(data["password"]):
+            raise ValidationError("Invalid username or password.")
+        return user
--- social_media_api/accounts/views.py
+++ social_media_api/accounts/views.py
@@ -1,15 +1,27 @@
 from django.shortcuts import render
-from rest_framework import generics
+from rest_framework import generics, status
 from rest_framework.authtoken.views import ObtainAuthToken
 from rest_framework.authtoken.models import Token
 from rest_framework.response import Response
-from .serializers import RegisterSerializer
+from .serializers import RegisterSerializer, LoginSerializer
 
 class RegisterView(generics.CreateAPIView):
     serializer_class = RegisterSerializer
 
-class LoginView(ObtainAuthToken):
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        user = serializer.save()
+        token = Token.objects.get(user=user)
+        return Response({"token": token.key}, status=status.HTTP_201_CREATED)
+
+
+class LoginView(generics.GenericAPIView):
+    serializer_class = LoginSerializer
+
     def post(self, request, *args, **kwargs):
-        response = super().post(request, *args, **kwargs)
-        token = Token.objects.get(user=response.data['user'])
-        return Response({'token': token.key})
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        user = serializer.validated_data
+        token, created = Token.objects.get_or_create(user=user)
+        return Response({"token": token.key}, status=status.HTTP_200_OK)
