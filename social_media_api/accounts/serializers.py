17da54effd3ae70555ce75cd68a0b4100081809c
Imaad Schuller <imaadschullerr@gmail.com>
2024-09-21 19:40:46 +0200

    Set up Django Social Media API with user authentication features

---
+++ social_media_api/accounts/serializers.py
@@ -0,0 +1,22 @@
+from rest_framework import serializers
+from django.contrib.auth import get_user_model
+from rest_framework.authtoken.models import Token
+
+User = get_user_model()
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ("id", "username", "bio", "profile_picture")
+
+class RegisterSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ("username", "password", "bio", "profile_picture")
+
+        def create(self, validated_data):
+            user = User(**validated_data)
+            user.set_password(validated_data["password"])
+            user.save()
+            Token.objects.create(user=user)
+            return user
---
+++ social_media_api/accounts/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+from rest_framework import generics
+from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.authtoken.models import Token
+from rest_framework.response import Response
+from .serializers import RegisterSerializer
+
+class RegisterView(generics.CreateAPIView):
+    serializer_class = RegisterSerializer
+
+class LoginView(ObtainAuthToken):
+    def post(self, request, *args, **kwargs):
+        response = super().post(request, *args, **kwargs)
+        token = Token.objects.get(user=response.data['user'])
+        return Response({'token': token.key})
