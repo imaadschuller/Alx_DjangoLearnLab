b787a1c5bb397b4a1d38cc75a351b12f4eabcc68
Imaad Schuller <imaadschullerr@gmail.com>
2024-09-22 11:15:23 +0200

    Edit security settings

--- social_media_api/social_media_api/settings.py
+++ social_media_api/social_media_api/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-b7#n1gy&!q9w(mp5a*$28%3#l*+v$y_s^&6(sx3)l7(%9fqscp'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
@@ -140,3 +140,9 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Security Settings
+SECURE_BROWSER_XSS_FILTER = True
+X_FRAME_OPTIONS = 'DENY'
+SECURE_CONTENT_TYPE_NOSNIFF = True
+SECURE_SSL_REDIRECT = True
